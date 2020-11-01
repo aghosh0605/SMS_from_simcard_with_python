ba46169160d107dde034ee48fd7852bd190e8b56
Aniruddha Ghosh <aghosh0605@gmail.com>
2020-11-02 01:52:27 +0530

    requirments
    req

---
+++ req_sms_send.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+#red=`tput setaf 1`
+#green=`tput setaf 2`
+#reset=`tput sgr0'
+
+
+
+
+echo "$(tput setaf 1)HERE WE GO AGAIN!! WE KNOW YOU ARE LAZY. SIT BACK AND RELAX......WE ARE SETTING UP THINGS FOR YOU :) $(tput sgr0)"
+
+sudo apt update
+sudo pip3 install pyairmore
+
+
+echo "$(tput setaf 1)DOWNLOAD AIRMORE HERE FOR YOUR MOBILE... https://airmore.com/  $(tput sgr0)"
+echo "$(tput setaf 2)YOU ARE ALL SET. GO AND CHILL..........  $(tput sgr0)"
+
+
+
+
+
+
